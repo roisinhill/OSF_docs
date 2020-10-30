07482d56f75b0b8a2d0b6130a110fee31592964c
roisinhill <r.hill2@nuigalway.ie>
2020-10-30 17:05:08 +0000

    Updated source code files

--- 1D_layer_adapted_mesh_r_refinement.py
+++ 1D_layer_adapted_mesh_r_refinement.py
@@ -4,7 +4,6 @@
 Generate 1-sided Bakhvalov-type mesh, for a singularly perturbed ODE
 whose solution has a layer near x = 0, by solving the MPDE
    -(rho(x) x'(xi))' = 0 for xi in (0,1) with x(0) = 0 and x(1) = 1,
-using uniform h-refinement.
 
 Gauss-Lobatto quadrature rule is used:
 i.e. replace 'return GaussJacobiQuadratureLineRule(ref_el, m)'
@@ -34,96 +33,67 @@ b = 0.99            # lower bound on reaction coefficient
 q = 0.5             # proportion of mesh points in layer
 K = q/(sigma*(1-q))
 
-# Parameters for h-refinement steps
-N_start = 4         # initial number of mesh intervals
-N_step = N_start    # current number of mesh intervals
-step_index = 0      # initial step number
-
 # Define right side of MPDE and initial solution
 f = Expression('0.0', degree=2)
 x_0 = Expression('x[0]', degree=2)
 
 # Define stopping criteria tolerance and intiial value
-residual_norm_TOL = 0.4
+residual_norm_TOL = 0.02
 residual_norm = residual_norm_TOL+1
 
-# Lists for meshes and computational function spaces
-mesh_list = []
-V_list = []
-list_length = int(math.log(N/N_start,2)+1)
-for i in range(0,list_length):
-    mesh_list.append("mesh%2d" %(i))
-    V_list.append("V2D%2d" %(i))
-
 # Computactional function space parameters
-def comp_space(N):
-    meshc = UnitIntervalMesh(N)
-    V = FunctionSpace(meshc, 'P', 1) # Function space with P1-elements
+def comp_space():
+    mesh = UnitIntervalMesh(N)
+    V = FunctionSpace(mesh, 'P', 1)  # Function space with P1-elements
     v = TestFunction(V)              # Test function on V
     x = TrialFunction(V)             # Trial function on V
-    return meshc, V, v, x
+    return mesh, V, v, x
 
-# FEM problem for the MPDE
-def MPDE_FEM(rho, xN, x, v, f, meshz, V):
-    bc = DirichletBC(V, x_0, 'on_boundary')      # Dirichlet boundary condition
-    a = rho(xN)*inner(grad(x),grad(v))*dx(meshz) # Left side of weak form
+# Define weak form of MMPDE
+def weak_form():
+    bc = DirichletBC(V, x_0, 'on_boundary')    # Dirichlet boundary condition
+    a = rho()*inner(grad(x),grad(v))*dx        # Left side of weak form
     L = f*v*dx                       # Right side of weak form
     xN = Function(V)                 # Numerical solution in V
     solve(a==L, xN, bc)              # Solve, applying the BCs strongly
     return xN
 
 # Define rho(x) for MPDE
-def rho(xN):
-    return conditional((K*(b/epsilon)*exp(-b*xN/(sigma*epsilon)))>1.0, \
-                           K*(b/epsilon)*exp(-b*xN/(sigma*epsilon)), 1.0)
+def rho():
+    return conditional((K*(b/epsilon)*exp(-b*xN/(sigma*epsilon)))>1.0, K*(b/epsilon)*exp(-b*xN/(sigma*epsilon)), 1.0)
 
-# The norm of the residual to use as stopping criterion
-def calc_residual(rho, V, meshN):
-    residual = assemble((rho(xN)*xN.dx(0)*v.dx(0))*dx) # residual of xN
+# calculate the norm of the residual to use as stopping criterion
+def calc_residual():
+    residual = assemble((rho()*xN.dx(0)*v.dx(0))*dx) # residual of xN
     bcr = DirichletBC(V, '0.0', 'on_boundary')
     bcr.apply(residual)                              # apply Dirichlet BCs
     residual_func = Function(V)
     residual_func.vector()[:] = residual.get_local() # Residual fn on V
-    residual_norm = norm(residual_func, 'L2', meshN) # L2-norm of residual
+    residual_norm = norm(residual_func, 'L2', mesh)  # L2-norm of residual
     return residual_norm
 
-# Initial function space parameters
-mesh_list[0], V_list[0], v, x = comp_space(N_step)
-xN = interpolate(x_0, V_list[0])                     # Initial value for xN
+mesh, V, v, x = comp_space()    # generate nesh and computational function space
+xN = interpolate(x_0, V)        # Initial value for xN
 
-# Iterate through uniform h-refinements
-while N_step < N:
-    for i in range(0,3):    # Calculate solution three times on each mesh size
-         xN = MPDE_FEM(rho, xN, x, v, f, mesh_list[step_index], \
-                               V_list[step_index])
+# Calculate solution and L2 norm of the residual
+iterations=0
+while residual_norm > residual_norm_TOL and iterations < N/2.0+5:
+    xN = weak_form()
+    residual_norm = calc_residual()
+    iterations +=1
 
-    N_step = N_step*2    # Double the number of mesh intervals
-    step_index = round(math.log(N_step/N_start,2))
-    # Generate computational function space on the finer mesh
-    mesh_list[step_index], V_list[step_index], v, x = comp_space(N_step)
-
-    # Interpolate the solution onto the new computational function space
-    xN = interpolate(xN, V_list[step_index])
-
-# Calculate solution and L2 norm of the residual on final function space
-while residual_norm > residual_norm_TOL:
-    xN = MPDE_FEM(rho, xN, x, v, f, mesh_list[step_index], V_list[step_index])
-    residual_norm = calc_residual(rho, V_list[step_index], \
-                               mesh_list[step_index])
-
-# Generate the physical mesh
 meshp = UnitIntervalMesh(N)
-meshp.coordinates()[:,0]  = xN.compute_vertex_values()[:]
+meshp.coordinates()[:,0] = xN.compute_vertex_values()
 
-# DEFINE REACTION-DIFFUSION PROBLEM TO SOLVE ON MESH GENERATED
+# DEFINE REACTION DIFFUSION PROBLEM TO SOLVE ON MESH GENERATED
 # Solve -epsilon^2 u" + u = 1-x for x in (0,1) with u(0) = u(1) = 0
 # Physical function space parameters
 Vp = FunctionSpace(meshp, 'P', 1)            # Function space with P1-elements
 vp = TestFunction(Vp)                        # Test function on V
 u = TrialFunction(Vp)                        # Trial function on V
-gp = Expression('x[0]*(1-x[0])', degree=2) # Boundary values
+gp = Expression('x[0]*(1-x[0])', degree=2)   # Boundary values
 bcp = DirichletBC(Vp, gp, 'on_boundary')     # Dirichlet boundary conditions
-fp= Expression('1.0-x[0]', degree=2)       # RHS of PDE
+fp= Expression('1.0-x[0]', degree=2)         # RHS of PDE
 
 # FEM problem for the reaction-diffusion equation
 a = epsilon*epsilon*dot(grad(u), grad(vp))*dx + u*vp*dx
--- 2D_layer_adapted_mesh_rh_refinement.py
+++ 2D_layer_adapted_mesh_rh_refinement.py
@@ -1,96 +1,102 @@
 ## 2D_layer_adapted_mesh_rh_refinement.py
-"""
+## FEniCS version: 2019.2.0.dev0
+'''
 Generate a non-tensor product layer adapted mesh, for a singularly
-perturbed PDE whose solution has layers that vary spatially near x = 1 and y = 1,
-and a corner layer near (x,y) = (1,1)
-by solving the MPDE -grad(M(x) grad(x(xi1, xi2))) = 0 for (xi1,xi2) in (0,1)^2 with
-x(0,xi2) = (0,x_xi2=0),
-x(1,xi2) = (1,x_xi2=0),
-x(xi1,0) = (x_xi1=0,0),
-x(xi1,1) = (x_xi1=0,1),
+perturbed PDE whose solution has layers that vary spatially near x = 1
+and y = 1, and meet at (1,1), by solving the MPDE
+    -grad(M(x) grad(x(xi1, xi2))) = (0,0)^T for (xi1,xi2) in (0,1)^2
+where x is a vector, with
+    x(0,xi2) = (0,x_xi2=0), x(1,xi2) = (1,x_xi2=0),
+    x(xi1,0) = (x_xi1=0,0), x(xi1,1) = (x_xi1=0,1),
 and using uniform h-refinements.
 
 This code is part of
-Mesh partial differential equations (PDEs): application and implementation,
-written by Roisin Hill and Niall Madden
-Contact: Roisin Hill
-For: <cite paper>
-"""
+Generating layer-adapted meshes using mesh partial differential equations,
+by Róisín Hill and Niall Madden. DOI:???
+Contact: Róisín Hill <Roisin.Hill@NUIGalway.ie>
+'''
 
 from fenics import *
 import math
 
 # Problem parameters
-epsilon = 1E-2 			# perturbation factor of the physical PDE
-N = 32  				# N+1 mesh points in each direction in final mesh
+epsilon = 1E-2      # perturbation factor of the physical PDE
+N = 32              # N+1 mesh points in each direction in final mesh
 
 # Parameters for h refinement steps
-N_start = 4				# initial mesh size
-N_step = N_start		# current mesh size
-step_index = 0			# initial step index
+N_start = 4         # initial mesh size
+N_step = N_start    # current mesh size
+step_index = 0      # initial step index
 
 # Parameters for M(x)
-sigma = 2.5				# dependant on method, often degree of elements +1 or +1.5
-b = 0.99				# minimum coefficient of reaction or convectio term as appropriate
-q = 0.5					# proportion of mesh points in layer
-K = q/(sigma*(1-q))		# indicates the proportion of points in the layer region
+sigma = 2.5 # dependant on method, often degree of elements +1 or +1.5
+b = 0.99    # minimum coefficient of reaction/convection term as appropriate
+q = 0.5     # proportion of mesh points in layer
+K = q/(sigma*(1-q))
 
 # define right side of MPDE
-f = Expression(('0.0','0.0'), degree = 2)
+f = Expression(('0.0','0.0'), degree=2)
 
-# Lists for mesh and function space names
-mesh_list =  ["mesh0", "mesh1","mesh2","mesh3","mesh4", "mesh5", "mesh6", "mesh7", "mesh8", "mesh9"]
-V_list = ["V0","V1","V2","V3","V4","V5","V6","V7","V8","V9"]
+# Lists for meshes and computational function spaces
+mesh_list = []
+V_list = []
+list_length = int(math.log(N/N_start,2)+1)
+for i in range(0,list_length):
+    mesh_list.append("mesh%2d" %(i))
+    V_list.append("V2D%2d" %(i))
 
 # generate computational function space
 def comp_space(N):
-	mesh = UnitSquareMesh(N, N, diagonal = "crossed") # uniform mesh on (0,1)^2
-	V = VectorFunctionSpace(mesh, 'P', 1)	# Function space V with linear elements
-	v = TrialFunction(V)					# Test function on V
-	x = TestFunction(V)						# Trial function on V
-	return mesh, V, v, x
+    mesh = UnitSquareMesh(N, N, diagonal='crossed') # uniform mesh on (0,1)^2
+    V = VectorFunctionSpace(mesh, 'P', 1)   # Function space V with P1 elements
+    v = TestFunction(V)                     # Test function on V
+    x = TrialFunction(V)                    # Trial function on V
+    return mesh, V, v, x
 
 # Define boundaries
 TOL = 1E-14
 def left_boundary( x, on_boundary):
-	return  abs(x[0]) < TOL
+    return  abs(x[0]) < TOL
 def right_boundary(x, on_boundary):
-	return abs(1-x[0]) < TOL
+    return abs(1-x[0]) < TOL
 def bottom_boundary( x, on_boundary):
-	return abs(x[1]) < TOL
+    return abs(x[1]) < TOL
 def top_boundary( x, on_boundary):
-	return abs(1-x[1]) < TOL
+    return abs(1-x[1]) < TOL
 
 # Define Dirichlet boundary conditions
 def boundary_conditions(V):
-	bcl = DirichletBC(V.sub(0), '0.0', left_boundary)
-	bcr = DirichletBC(V.sub(0), '1.0', right_boundary)
-	bcb = DirichletBC(V.sub(1), '0.0', bottom_boundary)
-	bct = DirichletBC(V.sub(1), '1.0', top_boundary)
-	bcs = [bcl, bcr, bcb, bct]
-	return bcs
+    bcl = DirichletBC(V.sub(0), '0.0', left_boundary)
+    bcr = DirichletBC(V.sub(0), '1.0', right_boundary)
+    bcb = DirichletBC(V.sub(1), '0.0', bottom_boundary)
+    bct = DirichletBC(V.sub(1), '1.0', top_boundary)
+    bcs = [bcl, bcr, bcb, bct]
+    return bcs
 
 # Define the matrix M(x) for MPDE
 def M(xN):
-	epsilon_y = Expression('epsilon*(1+xi1)*(1+xi1)', epsilon = epsilon, xi1 = xN.sub(0), degree = 2)
-	epsilon_x = Expression('epsilon*(2-xi2)*(2-xi2)', epsilon = epsilon, xi2 = xN.sub(1), degree = 2)
-	M = Expression((('K*(b/epsilon_x)*exp(-b*(1-xi1)/(sigma*epsilon_x))>1 ? K*(b/epsilon_x)*exp(-b*(1-xi1)/(sigma*epsilon_x)) : 1','0'),\
-	('0','K*(b/epsilon_y)*exp(-b*(1-xi2)/(sigma*epsilon_y))>1 ? K*(b/epsilon_y)*exp(-b*(1-xi2)/(sigma*epsilon_y)) :1')),\
-	K=K, b=b,sigma=sigma, epsilon_x = epsilon_x, epsilon_y = epsilon_y, xi1 = xN.sub(0), xi2 = xN.sub(1), degree = 4)
-	return M
+    epsilon_y = Expression('epsilon*(1+xi1)*(1+xi1)',\
+        epsilon=epsilon, xi1=xN.sub(0), degree=2)
+    epsilon_x = Expression('epsilon*(2-xi2)*(2-xi2)',\
+        epsilon=epsilon, xi2=xN.sub(1), degree=2)
+    M = Expression((('K*(b/epsilon_x)*exp(-b*(1-xi1)/(sigma*epsilon_x))>1 ? K*(b/epsilon_x)*exp(-b*(1-xi1)/(sigma*epsilon_x)) : 1','0'),\
+        ('0','K*(b/epsilon_y)*exp(-b*(1-xi2)/(sigma*epsilon_y))>1 ? K*(b/epsilon_y)*exp(-b*(1-xi2)/(sigma*epsilon_y)) :1')),\
+        K=K, b=b, sigma=sigma, epsilon_x=epsilon_x, epsilon_y=epsilon_y,\
+                   xi1=xN.sub(0), xi2=xN.sub(1), degree=4)
+    return M
 
-# Define the weak form
-def weak_form(M, xN, x, v, f, bcs, meshz, V):
-	a = inner(M(xN)*grad(x), grad(v))*dx(meshz) # Left side of weak weak_form
-	L = dot(f,v)*dx								# Right side of weak weak_form
-	xN = Function(V)							# Numerical solution in V
-	solve(a==L, xN, bcs)						# Solve, applying the DirichletBC strongly
-	return xN
+# Define the FEM problem for the MPDE
+def MPDE_FEM(M, xN, x, v, f, bcs, meshz, V):
+    a = inner(M(xN)*grad(x), grad(v))*dx(meshz) # Left side of weak form
+    L = dot(f,v)*dx                 # Right side of weak form
+    xN = Function(V)                # Numerical solution in V
+    solve(a==L, xN, bcs)            # Solve, applying the DirichletBC strongly
+    return xN
 
 # Initial function space parameters
 mesh_list[0], V_list[0], v, x = comp_space(N_step)
 
-# Set  intial value for xN: xN(xi1, xi2) = (xi1, xi2)
+# Set intial value for xN: xN(xi1, xi2) = (xi1, xi2)
 a = inner(grad(x),grad(v))*dx
 L = dot(f,v)*dx
 xN = Function(V_list[0])
@@ -98,27 +104,22 @@ solve(a==L, xN, boundary_conditions(V_list[0]))
 
 # Iterate through uniform h-refinements
 while N_step < N:
+    for i in range (0,4): # Solve MPDE 4 times on each mesh size
+        xN = MPDE_FEM(M, xN, x, v, f, boundary_conditions(V_list[step_index]), mesh_list[step_index], V_list[step_index])
 
-	# Generate solution four times on each mesh size
-	for i in range (0,4):
-		xN = weak_form(M, xN, x, v, f, boundary_conditions(V_list[step_index]), mesh_list[step_index], V_list[step_index])
+    N_step = N_step*2 # Double number of mesh intervals in each direction
+    step_index = round(math.log(N_step/N_start,2))
+    # Generate computational function space on the finer mesh
+    mesh_list[step_index], V_list[step_index], v, x = comp_space(N_step)
 
-	# Double the number of mesh intervals in each direction
-	N_step = N_step*2
-	step_index = round(math.log(N_step/N_start,2))
-
-	# Generate computational function space on the finer mesh
-	mesh_list[step_index], V_list[step_index], v, x = comp_space(N_step)
-
-	# Interpolate solution onto the new computational function space
-	xN = interpolate(xN,V_list[step_index])
+    # Interpolate solution onto the new computational function space
+    xN = interpolate(xN,V_list[step_index])
 
 # Calculate solution in final function space
 iterations = 0
 while iterations < 5:
-
-	xN = weak_form(M, xN, x, v, f, boundary_conditions(V_list[step_index]), mesh_list[step_index], V_list[step_index])
-	iterations +=1
+    xN = MPDE_FEM(M, xN, x, v, f, boundary_conditions(V_list[step_index]), mesh_list[step_index], V_list[step_index])
+    iterations +=1
 
 # Generate the physical mesh
 xiX, xiY = xN.split(True)
